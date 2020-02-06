3bd129298a766e690148b51942a0de4badd77666
katakura2305 <rinta2305@gmail.com>
2020-02-06 14:50:19 +0900

    add_Replay

---
+++ GameCreatorTGS2020/Assets/Script/WebCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCamera : MonoBehaviour
+{
+
+    public RawImage rawImage;
+
+    WebCamTexture webCamTexture;
+    // Start is called before the first frame update
+    void Start()
+    {
+        webCamTexture = new WebCamTexture();
+        rawImage.texture = webCamTexture;
+        webCamTexture.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ GameCreatorTGS2020/Assets/Script/WebCameraTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCameraTest : MonoBehaviour
+{
+    public RawImage rawImage;
+    public GameObject Image;
+
+    WebCamTexture webCamTexture;
+    bool on = false;
+
+    void Start()
+    {
+        webCamTexture = new WebCamTexture();
+        rawImage.texture = webCamTexture;
+    }
+
+    void Update()
+    {
+        if (on == false)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Image.SetActive(true);
+                webCamTexture.Play();
+                on = true;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("aaa");
+                Image.SetActive(false);
+                on = false;
+            }
+        }
+    }
+}
