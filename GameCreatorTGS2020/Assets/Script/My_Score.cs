4284dca78c0f8d5062225bd1d803adf1bc00a12e
katakura2305 <rinta2305@gmail.com>
2020-02-06 12:55:41 +0900

    add_Myscore

---
+++ GameCreatorTGS2020/Assets/Script/My_Score.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class My_Score : MonoBehaviour
+{
+    float ScoreNum;
+    int n;
+
+    void Start()
+    {
+        ScoreNum = 0;
+        n = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        ScoreNum = GetScore.Score;
+        //ScoreNum = ScoreNum + 1;
+
+        n = Mathf.FloorToInt(ScoreNum);
+
+        Text uiText = GetComponent<Text>();
+        uiText.text = "Score:"+n;
+    }
+}
