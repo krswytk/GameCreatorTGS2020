b94dd79c95203ec24d00312719ee76c1c92017e5
krswytk <krswytk.0228@gmail.com>
2020-02-06 14:13:10 +0900

    タイトル　時間でデモに移動　　 デモ　クリックでタイトルに移動

--- GameCreatorTGS2020/Assets/Script/Demo/TitleSwitchingClic.cs
+++ GameCreatorTGS2020/Assets/Script/Demo/TitleSwitchingClic.cs
@@ -3,22 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class TitleSwitching : MonoBehaviour
+public class TitleSwitchingClic : MonoBehaviour
 {
-    private float time;
-    public float go = 0;
     // Start is called before the first frame update
     void Start()
     {
-        time = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log(time);
-        time += Time.deltaTime;
-        if (time > go)
+        if (Input.GetMouseButton(0))
         {
             SceneManager.LoadScene("Title");
         }
