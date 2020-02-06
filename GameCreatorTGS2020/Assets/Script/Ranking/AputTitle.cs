d7e69c621dd57e2c1aec21fe91bcb0d701d8bbd5
krswytk <krswytk.0228@gmail.com>
2020-02-06 15:09:23 +0900

    全シーンにPでタイトルに移動するように移動 ランキング　Oでスイッチを切り替え　ONの場合タイトルに移動　OFFの場合メインに移動する　 Oのスイッチ切り替えは全シーンで可能

---
+++ GameCreatorTGS2020/Assets/Script/Ranking/AputTitle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AputTitle : MonoBehaviour
+{
+    private float time;
+    public float go = 0;
+    private bool TitleSwitch;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TitleSwitch = TitleSwhic.GetTitleSwotch();
+        time += Time.deltaTime;
+        if (time > go)
+        {
+            if (TitleSwitch == true)
+            {
+                SceneManager.LoadScene("Title");
+            }
+            else
+            {
+                SceneManager.LoadScene("Maine");
+            }
+        }
+    }
+}
