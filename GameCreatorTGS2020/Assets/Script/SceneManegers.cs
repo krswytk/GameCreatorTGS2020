0b7c6497464d9d76b98b54292e55ee777099dca5
krswytk <krswytk.0228@gmail.com>
2020-02-14 11:50:12 +0900

    時間経過ででもたいとる移動を実装

--- GameCreatorTGS2020/Assets/Script/SceneManegers.cs
+++ GameCreatorTGS2020/Assets/Script/SceneManegers.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class SceneManegers : MonoBehaviour
 {
-    static bool TitleSwitch = true;
-    static int SceneNumber;
+    static private bool TitleSwitch = true;
+    static private int SceneNumber;
+    static private float time;
+    public float outtime  = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,10 @@ public class SceneManegers : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+//////////////////////////////////////////////////////ここからボタンを使用したシーン移動についてのスクリプト
+///エンターもしくはマウス左クリックで次のシーンに移動
+///スペースでランキング後の移動シーンの変更　デフォルトはタイトル
+///左シフトでタイトルもしくはメインへの強制移動　移動先は上記記述を参照
         if(Input.GetKey(KeyCode.KeypadEnter) || Input.GetMouseButton(0))
         {
             SceneNumber++;
@@ -48,9 +53,35 @@ public class SceneManegers : MonoBehaviour
 
             SceneManager.LoadScene(SceneNumber);
         }
+        //////////////////////////////////////////////////////////////////////////ここまでキーマウスによるシーン移動
+        ///ここより各位ｓｃｒｉｐｔの時間経過による移動記述
+        time = Time.deltaTime;
+        if(SceneNumber == 0)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }//////////////ゲーム終了
+            if (time > outtime)
+            {
+                time = 0;
+                SceneManager.LoadScene(1);
+            }
+        }
+
+
+        if (SceneNumber == 1)
+        {
+            if (time > outtime)
+            {
+                time = 0;
+                SceneManager.LoadScene(0);
+            }
+        }
+        //////////////////////////////////////////ここまで時間経過によるタイトルとデモの移動
     }
 
-    static void SetTitleSwotch()
+    static void SetTitleSwotch()//////////////タイトルとメインへの切り替えボタン関数
     {
         if (TitleSwitch == false)
         {
@@ -62,7 +93,7 @@ public class SceneManegers : MonoBehaviour
         }
     }
 
-    public static bool GetTitleSwotch()
+    public static bool GetTitleSwotch()//////現在のタイトルメインへの切り替えを返す関数trueでタイトル
     {
         return TitleSwitch;
     }
