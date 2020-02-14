8048929f5adc2535d46e6b9fac9ad3ee1312cd69
ramobade <satou120120@gmail.com>
2020-02-14 18:02:02 +0900

    スコア画面の判定と音をつけました

--- GameCreatorTGS2020/Assets/Script/Score/My_Score.cs
+++ GameCreatorTGS2020/Assets/Script/Score/My_Score.cs
@@ -7,23 +7,63 @@ public class My_Score : MonoBehaviour
 {
     float ScoreNum;
     int n;
-
+    public AudioClip sound1;
+    public AudioClip sound2;
+    AudioSource audioSource;
+    private float time;
+    int count;
     void Start()
     {
         ScoreNum = 0;
         n = 0;
+        audioSource = GetComponent<AudioSource>();
+        count = 1140;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        time += Time.deltaTime;
         ScoreNum = GetScore.Score;
         //ScoreNum = ScoreNum + 1;
 
         n = Mathf.FloorToInt(ScoreNum);
 
         Text uiText = GetComponent<Text>();
-        uiText.text = "きみのスコアは:"+n;
+        if (n > 1000)
+        {
+            uiText.text = "ハイスコア！";
+            audioSource.PlayOneShot(sound1);
+            if (time > 4.0f)
+            {
+                {
+                    uiText.text = "きみのスコアは:" + n;
+                    if (count >= 1140)
+                    {
+                        audioSource.PlayOneShot(sound2);
+                        count = 0;
+
+                    }
+                    else
+                    {
+                        count++;
+                    }
+                }
+            }
+        }
+            else
+            {
+                uiText.text = "きみのスコアは:" + n;
+                if (count >= 1140)
+                {
+                    audioSource.PlayOneShot(sound2);
+                    count = 0;
+
+                }
+                else
+                {
+                    count++;
+                }
+            }
+        }
     }
-}
