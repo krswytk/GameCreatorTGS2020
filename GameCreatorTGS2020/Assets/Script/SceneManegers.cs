7244ddaf2cc6b5d397f55d6fba9ae6842672031d
krswytk <krswytk.0228@gmail.com>
2020-02-14 11:09:18 +0900

    シーン移動のスクリプトを統一

---
+++ GameCreatorTGS2020/Assets/Script/SceneManegers.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManegers : MonoBehaviour
+{
+    static bool TitleSwitch = true;
+    static int SceneNumber;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SceneNumber = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(Input.GetKey(KeyCode.KeypadEnter) || Input.GetMouseButton(0))
+        {
+            SceneNumber++;
+            if(SceneNumber == 5)
+            {
+                if(TitleSwitch == true)
+                {
+                    SceneNumber = 0;
+                }
+                else
+                {
+                    SceneNumber = 2;
+                }
+            }
+            SceneManager.LoadScene(SceneNumber);
+        }else if (Input.GetKey(KeyCode.Space))
+        {
+            SetTitleSwotch();
+        }else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (TitleSwitch == true)
+            {
+                SceneNumber = 0;
+            }
+            else
+            {
+                SceneNumber = 2;
+            }
+
+            SceneManager.LoadScene(SceneNumber);
+        }
+    }
+
+    static void SetTitleSwotch()
+    {
+        if (TitleSwitch == false)
+        {
+            TitleSwitch = true;
+        }
+        else
+        {
+            TitleSwitch = false;
+        }
+    }
+
+    public static bool GetTitleSwotch()
+    {
+        return TitleSwitch;
+    }
+}
