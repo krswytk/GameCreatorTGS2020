18221e29e19e09a86f7d304cb9f497757106d83a
katakura2305 <rinta2305@gmail.com>
2020-02-20 12:38:54 +0900

    add_glavity

--- GameCreatorTGS2020/Assets/Script/Maine/Player_Move.cs
+++ GameCreatorTGS2020/Assets/Script/Maine/Player_Move.cs
@@ -9,6 +9,7 @@ public class Player_Move : MonoBehaviour
     GameObject Camera;
     Transform myTransform;
     public static  Vector3 pos;
+    public static float Player_pos;
 
     int hit = 0;
 
@@ -20,7 +21,8 @@ public class Player_Move : MonoBehaviour
     {
         myTransform = this.transform;
         pos = myTransform.position;
-        Speed = pos.y;
+        //Speed = pos.y;
+
     }
 
     // Update is called once per frame
@@ -28,27 +30,42 @@ public class Player_Move : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.A))
         {
-            Speed = Speed - 0.1f;
-            pos.y = Speed;
-            myTransform.position = pos;  // 座標を設定
+            //Speed = Speed - 0.1f;
+            //pos.y = Speed;
+            //myTransform.position = pos;  // 座標を設定
+            this.gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+        }
+        if (Input.GetKey(KeyCode.B))
+        {
+            Num++;
         }
 
         if (Score == 0)
         {
             Score = GetScore.Score;
+            Num = Mathf.Floor((Score - 1000) / 100);
         }
         else
         {
             if (hit < Num)
             {
-                Speed = Speed - 0.1f;
-                pos.y = Speed;
-                myTransform.position = pos;  // 座標を設定
+                this.gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;
+                //Speed = Speed - 1.4f;
+                //pos.y = Speed;
+                //myTransform.position = pos;  // 座標を設定
+            }
+            if (hit == Num)
+            {
+                this.gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+                Rigidbody2D rb = this.GetComponent<Rigidbody2D>();  // rigidbodyを取得
+                Vector3 force = new Vector3(0.0f, -10.0f, 1.0f);    // 力を設定
+                rb.AddForce(force);
+                //Debug.Log("fack");
             }
         }
-
-        Num = Mathf.Floor((Score - 1000) / 100);
+        //Debug.Log(myTransform.position.y);
         //Debug.Log(Num);
+        Player_pos = myTransform.position.y;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
