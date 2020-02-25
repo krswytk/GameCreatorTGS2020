0fb2b6b39ee8e5196d916bb004336f8ed7e6781b
katakura2305 <rinta2305@gmail.com>
2020-02-25 14:08:45 +0900

    HitStop

--- GameCreatorTGS2020/Assets/Script/Maine/Break_anim.cs
+++ GameCreatorTGS2020/Assets/Script/Maine/Break_anim.cs
@@ -23,7 +23,10 @@ public class Break_anim : MonoBehaviour
 
     CameraShake shake;
 
-    
+    [SerializeField]
+    private GameObject damagePrefab;
+    [SerializeField]
+    private TimeManager timeManager;
 
     // Start is called before the first frame update
     void Start()
@@ -68,10 +71,15 @@ public class Break_anim : MonoBehaviour
 
         if (sound_on == false)
         {
-            if (collision.gameObject.tag == "Player")
+            if (collision.tag == "Player")
             {
                 if (Player_Move.break_stop == false)
                 {
+                    var damageParticle = GameObject.Instantiate(damagePrefab, collision.transform.position, Quaternion.identity) as GameObject;
+
+                    //　全体のタイムスケールを変更する
+                    timeManager.SlowDown();
+
                     //shake.Shake(1.25f, 2.1f);
                     MainSpriteRenderer.sprite = After;
                     sound_on = true;
---
+++ GameCreatorTGS2020/Assets/Script/Maine/HitStop.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitStop : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject damagePrefab;
+    [SerializeField]
+    private TimeManager timeManager;
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Player")
+        {
+
+            var damageParticle = GameObject.Instantiate(damagePrefab, col.ClosestPointOnBounds(col.transform.position), Quaternion.identity) as GameObject;
+
+            //　全体のタイムスケールを変更する
+            timeManager.SlowDown();
+        }
+    }
+}
+
