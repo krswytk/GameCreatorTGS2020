44da1b5fbbde34653003695de403e30bd2fadde2
krswytk <krswytk.0228@gmail.com>
2020-02-25 16:59:27 +0900

    修正②

---
+++ GameCreatorTGS2020/Assets/Script/ManagerScene/StartMaine.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StartMaine : MonoBehaviour
+{
+    private GameObject gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.Find("Arudino_Manager");
+        gm.GetComponent<GetScore>().Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
